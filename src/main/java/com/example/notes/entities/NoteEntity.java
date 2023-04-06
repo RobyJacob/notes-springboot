a1385e148c1d4c075f104f8bb8faf7dfd2c05cf6
roby <robyjacob1998@gmail.com>
2023-04-07 00:13:05 +0530

    connect database and create entity

--- src/main/java/com/example/notes/entities/NoteEntity.java
+++ src/main/java/com/example/notes/entities/NoteEntity.java
@@ -1,21 +1,14 @@
 package com.example.notes.entities;
 
+import com.example.notes.commons.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Table;
-import org.springframework.data.annotation.Id;
 
-@Entity(name = "note")
+@Entity(name = "notes")
 @Getter
 @Setter
-public class NoteEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Integer id;
-
+public class NoteEntity extends BaseEntity {
     private String title;
 
     private String body;
